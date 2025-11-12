035f67e01ad9ecb963f340e24e06a0b6b3a95400
Eric Hall <ewhall18@gmail.com>
2025-11-12 21:23:59 +0000

    sim:turns out MemCpyPioDevice isn't needed for build?

--- src/dev/acc/MemCpyAccel.py
+++ src/dev/acc/MemCpyAccel.py
@@ -1,14 +1,19 @@
+from m5.objects.Device import (
+    BasicPioDevice,
+    DmaDevice,
+)
 from m5.params import *
-from m5.objects.Device import DmaDevice, BasicPioDevice
+
 
 class MemCpyAccel(DmaDevice):
-    type = 'MemCpyAccel'
+    type = "MemCpyAccel"
     cxx_header = "dev/acc/memcpyaccel.hh"
     cxx_class = "gem5::MemCpyAccel"
     pioAddr = Param.Addr(0xC0000000, "Base address for PIO registers")
     pio_size = Param.Addr(0x20, "Size of PIO register space")
 
-class MemCpyPioDevice(BasicPioDevice):
-    type = 'MemCpyPioDevice'
-    cxx_header = "dev/acc/memcpyaccel.hh"
-    cxx_class = "gem5::MemCpyPioDevice"
+
+# class MemCpyPioDevice(BasicPioDevice):
+#     type = 'MemCpyPioDevice'
+#     cxx_header = "dev/acc/memcpyaccel.hh"
+#     cxx_class = "gem5::MemCpyPioDevice"
